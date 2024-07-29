bd85384eb6c97a892505602093aeb18f7bcf9ec0
973Karljoy <karljoyeux@gmail.com>
2024-07-29 22:23:22 +0200

    my learning state of zig the 29-07-2024

--- src/main.zig
+++ src/main.zig
@@ -5,10 +5,29 @@ pub fn main() !void {
     std.debug.print("Hello {s}!", .{"World"});
     // ***** -  Here is the begining of the language feature - ******//
     // Assignment
+    const i: i8 = 7;
+    std.debug.print(" - first printed number {any}", .{i});
+
+    var j: i8 = 2;
+    j = @as(i8, 3); //coercion type
     // Arrays
+    const m = [3]u5{ 1, 2, 3 };
+    std.debug.print(" - Size Of the Array m {any}", .{m.len});
+
+    const n = [_]i7{ 22, 3, 62, 43, 8 }; // we cant declare an array with 'var'
+    std.debug.print(" - Size Of the Array n {any}", .{n.len});
     // If Expressions
+    if (m.len < j) std.debug.print(" - m ({any}) est plus petite que j ({any}) ", .{ n.len, j });
+    if (m.len < i) std.debug.print(" - m ({any}) est plus petite que i ({any}) ", .{ n.len, i });
     // WhileLoops
+    while (j <= 5) {
+        std.debug.print(" - value of j : {any}", .{j});
+        j = j + 1;
+    }
     // ForLoops
+    for (n, 0..) |value, index| {
+        std.debug.print(" - Current Value = {any} at index {any}", .{ value, index });
+    }
     // Defer
     // Errors
     // Switch
