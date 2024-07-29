4a0d1be759978c7fa01501020715985d6c51b44f
973Karljoy <karljoyeux@gmail.com>
2024-07-29 21:27:55 +0200

    Discovering Zig and hanving fun

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,36 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("Hello {s}!",.{"World"});
-
-    
-
-
-
+    std.debug.print("Hello {s}!", .{"World"});
+    // ***** -  Here is the begining of the language feature - ******//
+    // Assignment
+    // Arrays
+    // If Expressions
+    // WhileLoops
+    // ForLoops
+    // Defer
+    // Errors
+    // Switch
+    // Pointers
+    // PointerSizedIntegers
+    // ManyItemPointers
+    // Slices
+    // Enums
+    // structs
+    // Union
+    // IntegerRules
+    // Floats
+    // labelledBlocks
+    // LabelLedLoops
+    // LoopsAsExpressions
+    // Optionals
+    // Comptime
+    // PayloadCaptures
+    // InlineLoops
+    // Opaque
+    // AnonymousStructs
+    // SentinelTerminaisons
+    // Vectors
+    // Imports
 
 }
-
